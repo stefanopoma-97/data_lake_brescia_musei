fb8d33608b476a59615e027196300ba0de53c838
Stefano <stefano1997poma97@gmail.com>
2022-09-13 10:42:09 +0200

    Controllo file basato su Spark

--- raw_zone.py
+++ raw_zone.py
@@ -10,7 +10,7 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from pyspark.sql import Row
-from pyspark.sql.functions import col, avg, to_date, from_unixtime, initcap, udf
+from pyspark.sql.functions import col, avg, to_date, from_unixtime, initcap, udf, input_file_name
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, DateType, TimestampType
 import os
@@ -19,18 +19,20 @@ import sys
 
 
 """
-Vengono lette tutte le opere (da file .csv). 
-Viene aggiunto il secolo, gli autori sono messi con la prima lettera maiuscola,
+Vengono lette tutte le opere (da file .csv).
+Struttura: 1;34455;Titolo 1;Tipologia 1;1900;Provenienza;autore1 cognome;1663053975
+Viene aggiunto il secolo, gli autori sono messi con la prima lettera maiuscola, viene derivata la data dal timestamp
 Viene inserito l'header e il nuovo dataframe viene salvato nella standardized zone.
 
-I file processati vengono inseriti nella sotto-cartella processed, in modo che non vengano processati ulteriormente
+I file processati vengono inseriti nella sotto-cartella processed, in modo che non vengano analizzati due volte
 """
 def opere_lista(spark):
     fileDirectory = 'raw/opere/lista/'
     moveDirectory = 'raw/opere/lista/processed/'
     destinationDirectory = 'standardized/opere/lista/'
-    files = os.listdir(fileDirectory)
+    #files = os.listdir(fileDirectory)
 
+    #schema del csv
     schema = StructType([ \
         StructField("ID", StringType(), True), \
         StructField("TagID", IntegerType(), True), \
@@ -48,22 +50,27 @@ def opere_lista(spark):
     #udf per estrarre secolo
     udfFunction_GetCentury = udf(Utilities.centuryFromYear)
 
-    #dmodifica del dataframe
+    #dmodifica del dataframe (inserita la data, il secolo e sistemato il campo autore)
     new = df.withColumn("Data creazione", from_unixtime("Timestamp"))\
             .withColumn("Autore", initcap("Autore"))\
             .withColumn("Secolo", udfFunction_GetCentury(df.Anno))
     new.show()
 
+
+    #shutil.move(fileDirectory + fname, moveDirectory + fname)
+
     #salvataggio
     os.makedirs(destinationDirectory, exist_ok=True)
-    new.write.mode("append").option("header", "true").option("delimiter",";").csv(destinationDirectory)
+    if (new.count()>0):
+        new.write.mode("append").option("header", "true").option("delimiter",";").csv(destinationDirectory)
 
-
+    #i file letti vengono spostati nella cartella processed
     os.makedirs(moveDirectory, exist_ok=True)
-    for fname in files:
-        if (os.path.isfile(fileDirectory + fname)):
-            print()
-            #shutil.move(fileDirectory + fname, moveDirectory +fname)
+    data = df.withColumn("input_file", input_file_name())
+    lista = data.select("input_file").rdd.flatMap(lambda x: x).collect()
+    for a in list(set(lista)):
+        fname = a.split("/")[-1]
+        shutil.move(fileDirectory + fname, moveDirectory + fname)
 
 
 
