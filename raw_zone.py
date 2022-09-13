dc08ea6bdaaa8b534c0f13a005502af8af6ec896
Stefano <stefano1997poma97@gmail.com>
2022-09-13 09:35:36 +0200

    letture CSV iniziale

--- raw_zone.py
+++ raw_zone.py
@@ -15,15 +15,41 @@ import os
 import shutil
 import sys
 
-def opere_lista():
+"""
+Vengono lette tutte le opere (da file .csv). 
+Viene aggiunto il secolo, gli autori sono messi con la prima lettera maiuscola,
+Viene inserito l'header e il nuovo dataframe viene salvato nella standardized zone.
+
+I file processati vengono inseriti nella sotto-cartella processed, in modo che non vengano processati ulteriormente
+"""
+def opere_lista(spark):
     fileDirectory = 'raw/opere/lista/'
     moveDirectory = 'raw/opere/lista/processed/'
     files = os.listdir(fileDirectory)
 
+    schema = StructType([ \
+        StructField("ID", StringType(), True), \
+        StructField("TagID", IntegerType(), True), \
+        StructField("Titolo", StringType(), True), \
+        StructField("Tipologia", StringType(), True), \
+        StructField("Anno", IntegerType(), True), \
+        StructField("Provenienza", StringType(), True), \
+        StructField("Autore", StringType(), True),\
+        StructField("Timestamp", IntegerType(), True)])
+
+    #legge tutti i file nella directory
+    df = spark.read.schema(schema).option("delimiter", ";").csv(fileDirectory)
+    df.printSchema()
+    df.show()
+
+
+
     os.makedirs(moveDirectory, exist_ok=True)
     for fname in files:
         if (os.path.isfile(fileDirectory + fname)):
-            shutil.move(fileDirectory + fname, moveDirectory +fname)
+            print()
+            #shutil.move(fileDirectory + fname, moveDirectory +fname)
+
 
 
 def main():
@@ -38,7 +64,7 @@ def main():
         enableHiveSupport(). \
         getOrCreate()
 
-    opere_lista()
+    opere_lista(spark)
 
 if __name__ == "__main__":
     main()
